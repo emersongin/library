1fc37e44fdfdb1dd6bab73175064799dd5476a9d
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-12-03 21:48:39 -0300

    feat: caso de uso create library user foi adicionado padrão repository de library user

--- src/usecase/CreateLibraryUser.php
+++ src/usecase/CreateLibraryUser.php
@@ -3,13 +3,21 @@
 namespace App\Library\UseCase;
 
 use App\Library\Domain\LibraryUser;
+use App\Library\Infra\Repository\LibraryUserRepository;
 
 final class CreateLibraryUser {
+  private LibraryUserRepository $libraryUserRepository;
+
+  public function __construct(LibraryUserRepository $libraryUserRepository) {
+    $this->libraryUserRepository = $libraryUserRepository;
+  }
+
   public function execute(array $input): array {
     $name = $input['name'];
     $cpf = $input['cpf'];
 
     $libraryUser = new LibraryUser($name, $cpf);
+    $this->libraryUserRepository->save($libraryUser);
 
     return [
       'id' => $libraryUser->getId()
